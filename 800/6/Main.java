a4a35355bff8898fde3db6dce4a2f1b4f7ea74d4
Ananya54321 <ananyapappula521@gmail.com>
2025-03-29 08:49:51 +0530

    6

---
+++ 800/6/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            HashMap<Integer, Integer> hm = new HashMap<>();
+            int ele = 0;
+            for (int i = 0; i < n; i++) {
+                ele = sc.nextInt();
+                hm.put(ele, hm.getOrDefault(hm, ele));
+            }
+            if (n == 2 || hm.size() == 1)
+                System.out.println("YES");
+            else if (hm.size() == 2) {
+                if (hm.get(ele) == n / 2 || hm.get(ele) == (n / 2) + 1) {
+                    System.out.println("YES");
+                } else
+                    System.out.println("NO");
+            } else
+                System.out.println("NO");
+        }
+    }
+}
